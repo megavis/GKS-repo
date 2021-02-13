c3920a8999739aee862c9170574730095d155606
megavis <58000641+megavis@users.noreply.github.com>
2021-02-13 23:32:47 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 provider "google" {
-  version = "3.48.0"
-  project = var.gcp_config.project
+  version = "3.56.0"
+  project_id = var.gcp_config.project_id
+  project = var.gcp_config.project_id
   region  = var.gcp_config.region
   zone    = var.gcp_config.zone
 }
