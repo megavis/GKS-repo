94e2c8c7cb3362cca5b5c99ff46e02afa014282b
megavis <58000641+megavis@users.noreply.github.com>
2021-02-14 22:39:25 +0200

    added worker-node

--- cks-cluster-init.tf
+++ cks-cluster-init.tf
@@ -21,3 +21,24 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "worker-node"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20201014"
+      size = 50
+    }
+  }
+
+  metadata_startup_script = "sudo bash -c 'bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh)'"
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
